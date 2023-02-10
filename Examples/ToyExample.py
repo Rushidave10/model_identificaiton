d64f8a88f66bbe15da7ab3ddc4fb9f057e4a286c
rushi10 <rushi.1996dave@gmail.com>
2023-02-10 06:48:34 +0100

    different example

--- Examples/ToyExample.py
+++ Examples/ToyExample.py
@@ -1,39 +1,38 @@
+import control as ct
 import matplotlib.pyplot as plt
 import numpy as np
-import control as ctl
 import control.optimal as opt
+import time
 
+A = np.array([[1, 0.5],
+              [0, 1]])
 
-m = 0.1  # mass of pendulum (kg)
-l = 0.5  # length of pendulum (m)
-g = 9.8  # acceleration due to gravity (m/s^2)
+B = np.array([[0.125],
+              [0.5]])
 
-# Define the state-space matrices
-A = np.array([[0, 1], [g/l, 0]])  # state transition matrix
-B = np.array([[0], [1/(m*l)]])    # input matrix
-C = np.array([[1, 0]])            # output matrix
-D = np.array([0])                 # feedforward matrix
+C = np.diag(np.ones(2))
+x0 = np.array([[3],
+               [0]])
 
-# Define the state-space model
-sys = ctl.ss(A, B, C, D, 0.2)
-N = 20  # prediction horizon
+sys = ct.ss(A, B, C, 0, 0.5)
+model = ct.ss2io(sys)
+in_constraints = opt.input_range_constraint(sys, -0.5, 0.5)
+state_constraints = opt.state_range_constraint(sys, [0, -1], [0, 1])
+all_constraints = [in_constraints, state_constraints]
 
-# Define the weight matrices for the cost function
-Q = np.array([[1, 0], [0, 0]])  # state weight matrix
-R = np.array([1])               # input weight matrix
+Q = np.diag(np.ones(2))
+R = np.array([1])
+cost = opt.quadratic_cost(sys=sys, Q=Q, R=R, x0=x0, u0=np.array([0]))
+timepts = np.linspace(0, 10, 21, endpoint=True)
+ctrl = opt.create_mpc_iosystem(model, timepts, cost, all_constraints)
 
-# Define the constraints on the states and inputs
-xmin = np.array([-np.pi, -10])
-xmax = np.array([np.pi, 10])
-umin = np.array([-20])
-umax = np.array([20])
+loop = ct.feedback(sys, ctrl, 1)
 
-# Define the initial state of the pendulum
-x0 = np.array([np.pi, 0])
+Nsim = 60
+tout, xout = ct.input_output_response(loop, timepts, X0=x0)
 
-# Define the MPC controller
-constraints = [opt.input_range_constraint(sys, [-20], [20])]
-cost = opt.quadratic_cost(sys, Q, R, x0=x0)
-ctrl = opt.create_mpc_iosystem(sys, np.arange(0, 6)*0.2, cost, constraints)
+for i, y in enumerate(C@xout):
+    plt.plot(tout, y)
+plt.title('outputs')
+plt.show()
 
-loop = ctl.feedback(sys, ctrl, 1)
