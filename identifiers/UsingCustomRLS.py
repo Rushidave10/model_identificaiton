760081887790076d2e31b430eab62c5b89c6fd0a
rushi10 <rushi.1996dave@gmail.com>
2023-02-14 10:43:08 +0100

    Example file for customRLS

---
+++ identifiers/UsingCustomRLS.py
@@ -0,0 +1,112 @@
+import control as ct
+import matplotlib.pyplot as plt
+import numpy as np
+import control.optimal as opt
+
+import tools.WrapperRLS
+from tools import *
+
+A = np.array([[1, 0.5],
+              [0, 1]])
+
+B = np.array([[0.125],
+              [0.5]])
+
+C = np.diag(np.ones(2))
+Ts = 0.5
+x0 = np.array([3, 0])
+xf = np.array([-3, 0])
+
+sys = ct.ss(A, B, C, 0, Ts)
+model = ct.ss2io(sys)
+in_constraints = opt.input_range_constraint(sys, -0.5, 0.5)
+term_constraints = opt.state_range_constraint(sys, xf, xf)
+state_constraints = opt.state_range_constraint(sys, [-5, -1], [5, 1])
+all_constraints = [in_constraints, state_constraints]
+
+Q = np.diag([1, 1])
+R = np.array([0.5])
+S = Q
+traj_cost = opt.quadratic_cost(sys=sys, Q=Q, R=R, x0=x0, u0=np.array([0]))
+terminal_cost = opt.quadratic_cost(sys, S, 0, x0=xf, u0=[0])
+timepts = np.linspace(0, 10, 21, endpoint=True)
+result = opt.solve_ocp(sys, timepts, X0=x0, cost=traj_cost,
+                       constraints=all_constraints,
+                       return_states=True,
+                       terminal_constraints=term_constraints
+                       )
+t_eval = np.linspace(0, 5, 11, endpoint=True)
+resp = ct.input_output_response(sys, timepts,
+                                result.inputs, x0, t_eval=timepts)
+t, y, u = resp.time, resp.outputs, resp.inputs
+
+# Online RLS:
+
+x_estimator = tools.WrapperRLS.RLS(n=3, p=2, mu=0.99)
+
+log_pred_x1 = np.zeros(len(t))
+log_pred_x2 = np.zeros(len(t))
+for k in range(len(t)):
+    thruster = np.array(u[0][k])
+    states = y[:, k]
+    matx = np.array([states[0], states[1], thruster])
+    x_ = x_estimator.predict(matx, p=2)
+
+    if k != 20:
+        x1 = y[0, k + 1]
+        x2 = y[1, k + 1]
+    else:
+        x1 = y[0, k]
+        x2 = y[0, k]
+    x = np.array([x1, x2])
+    x_estimator.adapt(x, matx)
+
+    log_pred_x1[k] = x_[0]
+    log_pred_x2[k] = x_[1]
+
+print(x_estimator.w)
+A_pred = np.array([[x_estimator.w[0][0], x_estimator.w[0][1]],
+                   [x_estimator.w[1][0], x_estimator.w[1][1]]])
+print(A_pred)
+B_pred = np.array([[x_estimator.w[0][-1]],
+                   [x_estimator.w[1][-1]]])
+
+print(B_pred)
+
+sys_ = ct.ss(A_pred, B_pred, C, 0, Ts)
+
+
+plt.subplot(3, 1, 1)
+plt.plot(t, y[0])
+plt.plot(t, log_pred_x1, '--')
+plt.ylabel("Theta[rad]")
+plt.title("Angular Position")
+plt.plot(x0[0], 'ro', t[-1], xf[0], 'ro')
+
+plt.subplot(3, 1, 2)
+plt.plot(t, y[1])
+plt.plot(t, log_pred_x2, '--')
+plt.ylabel("Theta_dot[rad/s]")
+plt.title("Angular Velocity")
+
+plt.subplot(313)
+plt.plot(t, u.T)
+plt.ylabel("F[N]")
+plt.title("Thruster")
+plt.tight_layout()
+plt.show()
+
+HEATMAP = False
+if HEATMAP:
+    cmap = 'hot'
+    plt.subplot(121)
+    plt.imshow(A, cmap=cmap, interpolation='nearest')
+    plt.title("True system Matrix")
+    plt.colorbar()
+
+    plt.subplot(122)
+    plt.imshow(A_pred, cmap=cmap, interpolation='nearest')
+    plt.title("Estimated system Matrix")
+    plt.colorbar()
+    plt.tight_layout()
+    plt.show()
